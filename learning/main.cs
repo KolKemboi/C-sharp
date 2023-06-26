9ecb3b329d05f2ca32e02019d2ecd7acf976805f
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2023-06-26 07:05:58 -0700

    Update main.cs

--- learning/main.cs
+++ learning/main.cs
@@ -1,5 +1,7 @@
 using System;
 
+//this is my first C-sharp code, and well, have to use crappy comments such as these
+
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
